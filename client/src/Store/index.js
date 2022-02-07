b4807e2ad9ab24fbe4da452083091a7fd974fc6f
RocioGL33 <rochisolchis.08@gmail.com>
2022-02-07 18:31:00 -0300

    starting Front (store reducer components files)

---
+++ client/src/Store/index.js
@@ -0,0 +1,9 @@
+import { createStore, appyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducer from "../Reducer";
+
+export const store = createStore(
+  reducer,
+  composeWithDevTools(appyMiddleware(thunk))
+);

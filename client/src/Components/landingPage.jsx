f07e5d490f7ea988e6bf0e1f57d3c19442ce7527
RocioGL33 <rochisolchis.08@gmail.com>
2022-02-09 00:22:32 -0300

    Landing Page Reworked

--- client/src/Components/landingPage.jsx
+++ client/src/Components/landingPage.jsx
@@ -5,8 +5,10 @@ export default function LandingPage() {
   return (
     <div>
       <h1>Welcome to Pokemon Metaverse</h1>
-      <Link to="/Home"></Link>
-      <button>Join!</button>
+      <Link to="/home">
+        <button>Join!</button>
+      </Link>
+
       <h3>
         This Individual Project was created by Rocio Garcia for Henry Full Stack
         Developer Academy, hope you like it.

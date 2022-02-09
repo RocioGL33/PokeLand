886918177c7a4d35f1c06e5ce5d3a3a585d409c5
RocioGL33 <rochisolchis.08@gmail.com>
2022-02-09 00:22:17 -0300

    Some Paths DONE

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
-import { Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./Components/landingPage";
+import Home from "./Components/home";
+import Detail from "./Components/detail";
 
 function App() {
   return (
-    <Routes>
-      <div className="App">
-        <h1>Henry Pokemon</h1>
-      </div>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/pokemons/:id" element={<Detail />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

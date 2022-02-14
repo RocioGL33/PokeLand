738514e5ca9c188fbcbccea6157e44c719a69932
RocioGL33 <rochisolchis.08@gmail.com>
2022-02-14 15:34:04 -0300

    Changes in Component Detail

--- client/src/Components/detail.jsx
+++ client/src/Components/detail.jsx
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getPokemonDetail } from "../Actions";
-import NavBar from "./navBar";
 
 export default function Detail() {
   const dispatch = useDispatch();
@@ -13,19 +12,20 @@ export default function Detail() {
     dispatch(getPokemonDetail(id));
   }, []);
 
-  const detail = useSelector((store) => store.pokemonDetail);
+  const detail = useSelector((state) => state.pokemonDetail);
 
   return (
-    <div>
-      <NavBar />
+    <>
       <img src={detail.img} alt="img not found" />
 
       <h2>{detail.name}</h2>
 
-      {detail && detail.types?.map((t) => <p>{t.name}</p>)}
+      {detail.types?.map((type) => (
+        <h3>{type.name}</h3>
+      ))}
       <div>
         <h4> ID: {detail.id}</h4>
-        <h4> HP: {detail.hp}</h4>
+        <h4> Hp: {detail.hp}</h4>
         <h4> Height: {detail.height}</h4>
         <h4> Weight: {detail.weight}</h4>
         <h4> Attack: {detail.attack}</h4>
@@ -35,6 +35,6 @@ export default function Detail() {
       <Link to="/home">
         <button>Back to home</button>
       </Link>
-    </div>
+    </>
   );
 }

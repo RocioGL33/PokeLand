338b990b6a235aafff46e5545eff9a46742ae9da
RocioGL33 <rochisolchis.08@gmail.com>
2022-02-14 15:34:38 -0300

    Form uncontrolled working

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Components/landingPage";
 import Home from "./Components/home";
 import Detail from "./Components/detail";
+import PokeCreated from "./Components/pokeCreated";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/pokemons/:id" element={<Detail />}></Route>
+        <Route path="/create" element={<PokeCreated />}></Route>
       </Routes>
     </BrowserRouter>
   );
